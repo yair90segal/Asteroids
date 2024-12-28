82387f9f6107306b147637085178ebe3ebde2e90
yair90segal <184441219+yair90segal@users.noreply.github.com>
2024-12-28 10:46:52 +0200

    Added fill surface inside main.py infinite while loop

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (255, 255, 255))
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
