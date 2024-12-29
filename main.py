920688ecc17d60b578fe79deb32e091ade47196f
yair90segal <184441219+yair90segal@users.noreply.github.com>
2024-12-30 01:35:25 +0200

    added groups updatable and drawable in main, plus functionality of drawing and updating to the player (and using it in main)

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        p.draw(screen)
+        for i in updatable:
+            i.update(dt)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
         dt_undevided = clock.tick(60)
         dt = dt_undevided / 1000
